b7d73fefba91e5e8bd1f1548882555a351acd295
Andrew Krause <andrew.l.krause@gmail.com>
2019-02-02 19:54:29 -0600

    change to static site

--- partials/_footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <small>&copy; A. Krause Design 2017</small>
-    <div class='social'>
-      <a href=''>Twitter</a>
-      <a href=''>Medium</a>
-      <a href=''>Dribbble</a>
-      <a href=''>Instagram</a>
-    </div>
-</footer>
